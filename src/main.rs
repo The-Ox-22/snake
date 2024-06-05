49159d923db60be98ee3056961a4de4c12b81a92
The-Ox-22 <iwf777@hotmail.com>
2024-06-04 21:40:55 -0500

    working with cors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use actix_files::Files;
-use actix_web::{web, App, HttpServer, HttpResponse, Result};
+use actix_cors::Cors;
+use actix_web::{web, App, HttpServer, HttpResponse};
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Position {
     row: usize,
     column: usize,
@@ -21,11 +21,16 @@ async fn main() -> std::io::Result<()> {
     });
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header();
+        
         App::new()
+            .wrap(cors)
             .app_data(app_state.clone())
-            .route("/api/position", web::get().to(get_position))
-            .route("/api/move", web::post().to(move_square))
-            .service(Files::new("/", "./build").index_file("index.html"))
+            .route("/position", web::get().to(get_position))
+            .route("/move", web::post().to(move_square))
     })
     .bind("127.0.0.1:8080")?
     .run()
@@ -34,11 +39,13 @@ async fn main() -> std::io::Result<()> {
 
 async fn get_position(data: web::Data<AppState>) -> HttpResponse {
     let position = data.position.lock().unwrap();
+    println!("Returning position: {:?}", *position);
     HttpResponse::Ok().json(&*position)
 }
 
 async fn move_square(data: web::Data<AppState>, movement: web::Json<Movement>) -> HttpResponse {
     let mut position = data.position.lock().unwrap();
+    println!("Received move request: {:?}", movement.direction);
     match movement.direction.as_str() {
         "ArrowUp" => if position.row > 1 { position.row -= 1 },
         "ArrowDown" => if position.row < 10 { position.row += 1 },
@@ -46,6 +53,7 @@ async fn move_square(data: web::Data<AppState>, movement: web::Json<Movement>) -
         "ArrowRight" => if position.column < 10 { position.column += 1 },
         _ => (),
     }
+    println!("New position: {:?}", *position);
     HttpResponse::Ok().json(&*position)
 }
 
